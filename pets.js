cb310e2a2683cc1b0e14111e1be799ccdd681c4c
Solomon <biasolo0611@gmail.com>
2025-03-21 17:26:35 +0200

    Update pets.js

--- pets.js
+++ pets.js
@@ -1,17 +1,19 @@
 // all pet data
 const pets = [
     {
-        id: ,
-        name: "",
-        type: "",
-        level: "",
-        expansions: ["", "", ""],
+        id: 1,
+        name: "Brown Silithid",
+        type: "Aqiri",
+        level: "19-59",
+        expansions: ["Wrath of the Lich King", "Cataclysm", "Mists of Pandaria"],
     locations: [
-        { expansion: "", place: "", level: "" },
-        { expansion: "", place: "", level: "" },
-        { expansion: "", place: "", level: "" }
+        { expansion: "Wrath of the Lich King", place: "Thousand Needles", level: "29 - 58" },
+        { expansion: "Wrath of the Lich King", place: "Temple of Ahn'Qiraj (Raid)", level: "31" },
+        { expansion: "Cataclysm", place: "Thousand Needles", level: "19 - 59" },
+        { expansion: "Mists of Pandaria", place: "Ahn'Qiraj: The Fallen Kingdom, Silithus", level: "30" }
+        
     ],
-        img: ""
+        img: "https://www.wow-petopia.com/images/skin_pix/silithidtankskintan.jpg"
     },
     {
         id: ,
