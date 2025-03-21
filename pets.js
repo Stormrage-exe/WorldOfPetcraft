dd67eb9844cf9fa070ea768b919d7fbdf0f2fd26
Solomon <biasolo0611@gmail.com>
2025-03-21 14:10:26 +0200

    Added templates for pet info in pets.js.

--- pets.js
+++ pets.js
@@ -1,1 +1,417 @@
 // all pet data
+const pets = [
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: Spirit Beast",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    },
+    {
+        id: ,
+        name: "",
+        type: "",
+        expansion: "",
+        level: "",
+        location: "",
+        img: ""
+    }
+];
